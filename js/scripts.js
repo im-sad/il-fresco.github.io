7355845f7bf95ea36446c64ebb29d1a74f9bddbe
Andrew <im-sad@ya.ru>
2021-02-19 17:47:00 +0300

    stable version

--- js/scripts.js
+++ js/scripts.js
@@ -1,157 +1,90 @@
-const data = [
-  {
-    about: 'о зарплате',
-    text: 'Ну только если хуй в жопу, а бабки через год.'
-  },
-  {
-    about: 'о капитализме',
-    text: 'Если у компании нет денег и ничего более, то их нет.'
-  },
-  {
-    about: 'о времени',
-    text: 'Все произойдет ровно тогда когда сможет произойти.'
-  },
-  {
-    about: 'о самостоятельности',
-    text: 'Научись задавать правильные вопросы самому себе и будешь получать правильные ответы от других.'
-  },
-  {
-    about: 'об утрате ',
-    text: 'Потери уже свершились. Больше они уже быть не могут.'
-  },
-  {
-    about: 'о бесмысленности бытия',
-    text: 'Все уже написано 10 раз, вопросы заданы. Ответы я не получил.'
-  },
-  {
-    about: 'об ошибках',
-    text: 'Не ошибается только тот, кто ничего не делает.'
-  },
-  {
-    about: 'о готовности отдавать',
-    text: 'Жду вопросов и дам ответы.'
-  },
-  {
-    about: 'о тупике полемики',
-    text: 'Остановите поезд, я сойду.'
-  },
-  {
-    about: 'о зависти',
-    text: 'Тебе не понять, что такое обещание и ответсвенность.'
-  },
-  {
-    about: 'о человеческой глупости',
-    text: 'Ты самый тупой - это другое'
-  },
-  {
-    about: 'о способности делиться',
-    text: 'Все ради тебя, чтобы твою голову ебучую только зря не тревожить.'
-  },
-  {
-    about: 'о самодисциплине',
-    text: 'Оправдания, они как задница - найдутся у каждого'
-  },
-  {
-    about: 'о деньгах',
-    text: 'Деньги будут завтра, ну край в пятницу!'
-  },
-  {
-    about: 'о неприятелях',
-    text: 'Кто же из вас такой трус, такое ничтожное существо, что опускается до такого?'
-  },
-  {
-    about: 'об относительности',
-    text: 'Под определенным углом и земля плоская.'
-  },
-  {
-    about: 'о людях',
-    text: 'Вы живете в каком-то своем мире, полным предрассудков и заблуждений.'
-  },
-  {
-    about: 'о дедлайнах',
-    text: 'Сколько десятков раз я слушал: «будет готово в понедельник», и не было сделано НИКОГДА.'
-  },
-  {
-    about: 'про обязательства',
-    text: 'Взялся - тяни'
-  },
-  {
-    about: 'про вопросы',
-    text: 'Не всегда открытый вопрос описан предложением со знаком вопроса на конце.'
-  },
-  {
-    about: 'про новое поколение',
-    text: 'Вопросы ваши основаны на отсутствии понимания и молодости.'
-  },
-  {
-    about: 'про иллюзорность бытия',
-    text: 'Ты загоняешь себе в голову бессмысленный выдуманный мусор, а потом распространяешь его как правду'
-  },
-  {
-    about: 'про бумеров',
-    text: 'Мы чуть умнее чем вы думаете'
-  },
-  {
-    about: 'снисходителен',
-    text: 'Ты дурак? Совсем ебобо видимо'
-  },
-  {
-    about: 'о процессе человеческой деградации',
-    text: 'Банкротится мозг у тебя'
-  },
-  {
-    about: 'об экономике',
-    text: 'Чтобы пользоваться чьими-то деньгмаи - их надо сначала взять у человека. А деньгами которых нет - нельзя пользоваться'
-  },
-  {
-    about: 'о жизни',
-    text: 'У меня вообще все заебись, если честно'
-  },
-  {
-    about: 'об экзистенциальной составляющей',
-    text: 'В моменте'
-  },
-  {
-    about: 'держит всё под контролем',
-    text: 'Ну что там?'
-  },
-  {
-    about: 'о непостоянстве',
-    text: 'Хотя пиздеть ты горазд, а как до дела и фактов - сливаешься'
-  },
-  {
-    about: 'о ненадёжности',
-    text: 'Опять пропизделся'
-  },
-];
-
 const done = [];
 
 document.addEventListener('DOMContentLoaded', function() {
   const btn = document.getElementById('js-reload');
+  const share = document.getElementById('js-share');
   const about = document.getElementById('js-about');
   const content = document.getElementById('js-content');
+  const input = document.getElementById('js-link');
+  const alert = document.getElementById('js-done');
+  const allList = document.getElementById('js-all-list');
+  const menu = document.getElementById('js-menu');
+  const sidebar = document.getElementById('js-sidebar');
 
   btn.addEventListener('click', () => getCite());
 
-  getCite();
+  menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+  });
 
-  function getCite() {
-    const citeNum = randomInteger(0, data.length - 1, done);
+  share.addEventListener('click', (e) => getLink(e));
 
-    console.log(done)
-    console.log(citeNum)
+  createList();
+  checkLocation();
 
-    done.push(citeNum)
+  window.addEventListener('hashchange', () => {
+    const hash = parseInt(location.hash.match(/\d+/));
+    return getCite(hash, true);
+  });
+
+
+  function createList() {
+    let list = '';
+
+    for (var i = 0; i < data.length; i++) {
+      list +=  `<li><a href="#${i}">${data[i].about}</a></li>`;
+    }
+
+    allList.innerHTML = list;
+  }
+
+  function checkLocation() {
+    const hash = parseInt(location.hash.match(/\d+/));
+    return hash ? getCite(hash) : getCite();
+  }
+
+  function getCite(num, nostate) {
+    let citeNum;
+
+    num || num === 0 ? citeNum = num : citeNum = randomInteger(0, data.length - 1, done);
+
     about.innerHTML = data[citeNum].about;
     content.innerHTML = data[citeNum].text;
+    share.href = `#cite-${citeNum}`;
+    input.value = document.location;
 
+    if (!nostate) {
+      done.push(citeNum);
+      history.pushState(null, null, `#cite-${citeNum}`);
+    }
 
     if (done.length >= data.length) {
-      btn.style.display = 'none'
+      btn.style.visibility = 'hidden';
+      share.style.visibility = 'hidden';
       about.innerHTML = 'говорит';
-      content.innerHTML = 'Мне больше нечего сказать';
+      content.innerHTML = 'Всем мир!';
+      history.pushState(null, null, '#');
+      done.length = 0;
+    } else {
+      btn.style.visibility = 'visible';
+      share.style.visibility = 'visible';
     }
+
+    return citeNum;
+  }
+
+  function getLink(e) {
+    const anchor = e.currentTarget.href;
+    const url = document.location;
+
+    input.select();
+    document.execCommand('copy');
+
+    alert.classList.add('is-visible');
+    let timer = setTimeout(() => {
+      alert.classList.remove('is-visible');
+    }, 1000)
   }
 });
 
@@ -159,7 +92,8 @@ document.addEventListener('DOMContentLoaded', function() {
 function randomInteger(min, max, exp) {
     var n,
         exp = Array.isArray(exp) ? exp : [(isNaN(exp) ? min-1 : exp)];
-    while(true){
+
+    while(true) {
         n = Math.floor(Math.random() * (max - min + 1)) + min;
         if(exp.indexOf(n) < 0) return n;
     }
